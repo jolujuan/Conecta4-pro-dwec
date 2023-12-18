13c8c9369f5533fa5cf9b2b8c417c90d81d91f10
jolujuan <thesameexample@outlook.es>
2023-12-18 02:34:07 +0100

    desplegament

--- src/views/footer.js
+++ src/views/footer.js
@@ -3,10 +3,10 @@ export { footer };
 const footer =() => `<div class="footer-bottom">
     <div class="footer-bottom-main">
         <div class="footer-social-icons">
-            <a href="#"> <img src="./img/icons/instagram.svg"></a>
-            <a href="#"> <img src="./img/icons/facebook.svg"></a>
-            <a href="#"> <img src="./img/icons/youtube.png"></a>
-            <a href="#"> <img src="./img/icons/gorjeo.svg"></a>
+            <a href="#"> <img src="./src/img/icons/instagram.svg"></a>
+            <a href="#"> <img src="./src/img/icons/facebook.svg"></a>
+            <a href="#"> <img src="./src/img/icons/youtube.png"></a>
+            <a href="#"> <img src="./src/img/icons/gorjeo.svg"></a>
 
         </div>
         <div class="footer-copyright">
